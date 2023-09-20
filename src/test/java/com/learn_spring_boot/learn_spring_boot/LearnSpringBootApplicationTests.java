1e6c4de5b8aeda43aa493f75906d23e27b9d8c75
phanquanDHTB <92263327+phanquanDHTB@users.noreply.github.com>
2023-09-20 09:53:23 +0700

    first commit

---
+++ src/test/java/com/learn_spring_boot/learn_spring_boot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_spring_boot.learn_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
